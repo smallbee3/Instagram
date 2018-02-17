0bbf8933c96e63b10af1959f4a9feca51cc761a2
smallbee <smallbee3@gmail.com>
2018-02-17 10:28:04 +0900

    02. Post, Comment model

--- instagram/config/settings.py
+++ instagram/config/settings.py
@@ -40,8 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Thirdparty App
+    'django_extensions',
+
     # Custom App
     'member.apps.MemberConfig',
+    'post.apps.PostConfig',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Media files (User uploaded files)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
---
+++ instagram/post/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from config import settings
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='post')
+
+    def __str__(self):
+        return f'Post (PK: {self.pk}, Author: {self.author.username})'
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return f'Comment (PK: {self.pk}, Author: {self.author.username})'
